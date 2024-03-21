6d6314ae4fee7a297ba813f384595b07b4ed18a0
shahid1170 <96632432+shahid1170@users.noreply.github.com>
2024-03-21 23:38:22 +0500

    quiz new

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,39 @@
 import { useState } from 'react'
-import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-import { count } from 'console';
+// import './App.css'
+import "./mystyle.css"
 
+import {result} from "./mylib/data"
 
-export default function App(){
-  let [counter, update]=useState(0);
- const increment=() =>{
-  update(counter +1 );
- }
- return (
-    <>
-    <h2>Counter</h2>
-    <h2>counter={counter}</h2>
-    <button onClick={increment}>Click here</button>
-    </>
+function App() {
+  const marks = [60, 40, 90, 70, 34, 41, 28, 50, 88, 100, 66];
+  const {pass, fail} =result(marks); 
+  // const stylePass ={color:"white", backgroundColor:"black"}
+  return(
+   <><button type="button" className="btn btn-primary">Primary</button>
+   <button type="button" className="btn btn-secondary">Secondary</button>
+   <button type="button" className="btn btn-success">Success</button>
+   <button type="button" className="btn btn-danger">Danger</button>
+   <button type="button" className="btn btn-warning">Warning</button>
+   <button type="button" className="btn btn-info">Info</button>
+   <button type="button" className="btn btn-light">Light</button>
+   <button type="button" className="btn btn-dark">Dark</button>
+   
+   <button type="button" className="btn btn-link">Link</button>
+   <br />   <br /> <br />
+   <h3 className="pass">Passing Sheet({pass.length})</h3>
+   <ul>{
+    pass.map((m,i)=><li key={i}>{m}</li>)
+  }  
+   </ul> 
+   <h3 classnameName="fail">Failing Sheet({fail.length})</h3>
+   <ul>{
+   fail.map((m,i)=><li key={i}>{m}</li>)
+   }
+   </ul>
+   </> 
   )
- }
+}
 
-  // const myFunction=(name) => {
-  //   alert(`Welcome dear ${name}`);
-  // }
-  // return(
-  //   <>
-  //   <h2>Welcome message</h2>
-  //   <button onClick={() =>myFunction("TAHIR")}>Click here</button>
-  //   </>
-  // )
-  
-
+export default App;
