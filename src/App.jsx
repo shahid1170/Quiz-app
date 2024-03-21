3be3a46e7d9bbc8401a03e6565f7e3eb853eacf0
shahid1170 <96632432+shahid1170@users.noreply.github.com>
2024-03-21 20:56:22 +0500

    quiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
 import { useState } from 'react'
+import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-// import "./mystyle.css"
+import { count } from 'console';
 
-// import {result} from "./mylib/data"
-import { product } from './data/products'
-import KfcHeader from './components/header'
-import ImgTop from './components/topimg'
-import ImgCard from './components/card'
 
-function App() {
-
-  return (
-    <div>
-     <KfcHeader className="bottom"/>
-      <ImgTop className="bottom"/>
-      <ImgCard class card/>       
-    </div> 
-  
-    
-
+export default function App(){
+  let [counter, update]=useState(0);
+ const increment=() =>{
+  update(counter +1 );
+ }
+ return (
+    <>
+    <h2>Counter</h2>
+    <h2>counter={counter}</h2>
+    <button onClick={increment}>Click here</button>
+    </>
   )
-}
+ }
 
-export default App;
+  // const myFunction=(name) => {
+  //   alert(`Welcome dear ${name}`);
+  // }
+  // return(
+  //   <>
+  //   <h2>Welcome message</h2>
+  //   <button onClick={() =>myFunction("TAHIR")}>Click here</button>
+  //   </>
+  // )
+  
+
