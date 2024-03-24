af8622200c1faadce7489dd37b94b860a2c63aca
shahid1170 <96632432+shahid1170@users.noreply.github.com>
2024-03-25 00:42:10 +0500

    select

--- src/App.jsx
+++ src/App.jsx
@@ -7,41 +7,66 @@ import "./mystyle.css"
 
 
 export default function App() {
-    let [grades, setGrades]= useState([
-      {id:1, marks:49, course:"C++"},
-      {id:2, marks:52, course:"Dart"},
-      {id:3, marks:48, course:"Python"},
-      {id:4, marks:53, course:"Angular"},
-    ])
-    
-    const increment=(index)=>{
-      const copyGrades=[...grades];
-      copyGrades[index].marks++;
-      setGrades(copyGrades);
+  let [grades, setGrades] = useState([
+    { id: 1, marks: 49, course: "C++" },
+    { id: 2, marks: 52, course: "Dart" },
+    { id: 3, marks: 48, course: "Python" },
+    { id: 4, marks: 53, course: "Angular" },
+    { id: 5, marks: 54, course: "Space" },
+  ])
 
-    }
-    const decrement=(index)=>{
-      const copyGrades=[...grades];
-      copyGrades[index].marks--;
-      setGrades(copyGrades);
-    }
-  return(
+  const increment = (index) => {
+    const copyGrades = [...grades];
+    copyGrades[index].marks++;
+    setGrades(copyGrades);
+
+  }
+  const decrement = (index) => {
+    const copyGrades = [...grades];
+    copyGrades[index].marks--;
+    setGrades(copyGrades);
+  }
+
+  const onDelete = (id) => {
+    setGrades(grades.filter(g => g.id !== id));
+
+  }
+  const onSelect = (id) => {
+    copyGrades=[...grades]
+    const course=copyGrades.find(g => g.id === id);
+    course.selected=ture;
+    setGrades(copyGrades);
+  }
+  return (
     <div className="container my-5">
       {
-      grades.map((g, i) =>(
-        <div key={g.id}>
-        <button onClick={() =>increment(i)}>+</button>
-       Your Marks are {g.marks} in {g.course} 
-        <button onClick={()=>decrement(i)}>-</button>
-        </div> 
-))}
+        grades.map((g, i) => (
+          <div key={g.id}>
+            <button onClick={() => increment(i)}>+</button>
+            Your Marks are {g.marks} in {g.course}
+            <button onClick={() => decrement(i)}>-</button>
+            <button onClick={() => onDelete(g.id)}>Delete</button>
+            <button onClick={()=> onSelect(g.id)}>Select</button>
+          </div>
+        ))}
+        <hr />
+        <h6>Selected Grades</h6>
+        {grades.filter(g=>g.selected).map((g, i) => (
+          <div key={g.id}>
+            <button onClick={() => increment(i)}>+</button>
+            Your Marks are {g.marks} in {g.course}
+            <button onClick={() => decrement(i)}>-</button>
+            
+            
+          </div>))}
+
     </div>
   )
 }
-  
-   
-   
-   
- 
+
+
+
+
+
 
 
